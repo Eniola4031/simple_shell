59ff533f8f313ca5c788bd46dea954dcf2990de3
eniola4031 <agboola.eniola4031@gmail.com>
2023-11-16 10:51:39 -0800

    update

--- shell.c
+++ shell.c
@@ -1,40 +1,51 @@
 #include "shell.h"
 
 int main() {
-  char command[100];
-  int status;
+    char command[100];
+    int status;
 
-  while (1) {
-    printf(PROMPT);
+    while (1) {
+        // Display the prompt
+        printf("simple_shell$ ");
 
-    // Get the user's command
-    if (fgets(command, sizeof(command), stdin) == NULL) {
-      if (feof(stdin)) {
-        printf("\n");
-        exit(0);
-      } else {
-        perror("fgets");
-        exit(1);
-      }
+        // Read the command from the user
+        if (!fgets(command, sizeof(command), stdin)) {
+            // Handle end of file condition (Ctrl+D)
+            printf("\n");
+            exit(0);
+        }
+
+        // Remove the trailing newline character
+        command[strlen(command) - 1] = '\0';
+
+        // Check if the command is empty
+        if (strlen(command) == 0) {
+            continue;
+        }
+
+        // Fork a child process
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            // Child process
+            execl(command, command, NULL, environ);
+
+            // If execl fails, print an error message and exit
+            printf("Error: Cannot execute command '%s'\n", command);
+            exit(1);
+        } else if (pid > 0) {
+            // Parent process
+            wait(&status);
+
+            if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+                printf("Error: Command '%s' exited with status %d\n", command, WEXITSTATUS(status));
+            }
+        } else {
+            // Fork failed
+            printf("Error: Fork failed\n");
+            exit(1);
+        }
     }
 
-    // Remove the newline character from the command
-    command[strlen(command) - 1] = '\0';
-
-    // Check if the command is an empty string
-    if (strlen(command) == 0) {
-      continue;
-    }
-
-    // Execute the command using execve
-    status = execve(command, NULL, NULL);
-
-    if (status == -1) {
-      printf("Command not found: %s\n", command);
-    } else {
-      printf("Error executing command: %s\n", command);
-    }
-  }
-
-  return 0;
+    return 0;
 }
