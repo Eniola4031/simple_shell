ee3566eece2730448936e53e8ddf39a016f45ea6
eniola4031 <agboola.eniola4031@gmail.com>
2023-11-16 13:15:51 -0800

    betty

---
+++ main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1024
+
+int main(void);
+
--- shell.c
+++ shell.c
@@ -1,30 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-#define MAX_LINE_LENGTH 1024
+/**
+ * main(): This function is the entry point for the simple_shell program.
+ * It prompts the user for a command, executes it, and handles errors.
+ *
+ * Return: always zero
+ */
+	int main(void)
+{
+		char line[MAX_LINE_LENGTH];
 
-int main() {
-  char line[MAX_LINE_LENGTH];
+		while (1)
+		{
+			printf("simple_shell> ");
 
-  while (1) {
-    printf("simple_shell> ");
-    fgets(line, MAX_LINE_LENGTH, stdin);
-
-    if (feof(stdin)) {
-      break;
-    }
-
-    if (strlen(line) <= 1) {
-      continue;
-    }
-
-    line[strlen(line) - 1] = '\0';  /* Remove the newline character */
-
-    if (system(line) != 0) {
-      printf("Command not found\n");
-    }
-  }
-
-  return 0;
+			fgets(line, MAX_LINE_LENGTH, stdin);
+			if (feof(stdin))
+				break;
+			if (strlen(line) <= 1)
+				continue;
+			line[strlen(line) - 1] = '\0';  /* Remove the newline character */
+			if (system(line) != 0)
+				printf("Command not found\n")
+		}
+		return (0);
 }
